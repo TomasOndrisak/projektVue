619a978b03b5e0579f6768416cad43a8c7c43556
TomasOndrisak <104638159+TomasOndrisak@users.noreply.github.com>
2022-05-24 11:51:16 +0200

    zmazanie archivovanych a pridanie tsconfig

--- src/Http-api.js
+++ src/Http-api.js
@@ -2,6 +2,6 @@
     import axios from "axios";  
       
     export default axios.create({  
-      baseURL: "https://localhost:49153",  
+      baseURL: "https://localhost:5001",  
      
     });  
